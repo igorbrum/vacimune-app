f8d0f9a3c81570d569c1b64e2e903654d9ca4003
Igor Brum <igorsbrum@gmail.com>
2018-05-22 16:51:06 -0300

    Add Custom Theme Material Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
